70b7a1b88e0be491e8d55b03c4bbe07370920f97
Siva M <murugansiva1260@gmail.com>
2026-05-12 22:01:46 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node.js DevOps Projectssssssssss 🚀");
+    res.send("Hello from Node.js DevOps Projectsss 🚀");
 });
 
 app.get('/health', (req, res) => {
